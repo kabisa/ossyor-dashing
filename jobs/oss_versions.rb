c7dae5b9863643aea1660a46e9a88a315a870f42
Matthijs Groen <matthijs.groen@gmail.com>
2013-11-08 14:09:31 +0100

    Add demo env to watch list

--- jobs/oss_versions.rb
+++ jobs/oss_versions.rb
@@ -5,7 +5,7 @@ require 'date'
 SCHEDULER.every '10m', :first_in => 0 do |job|
   # http://www.pwl.philips.com/
 
-  environments = %w(pwl content.pwl staging.pwl)
+  environments = %w(pwl content.pwl staging.pwl demo.pwl)
   versions = []
 
   environments.each do |environment|
