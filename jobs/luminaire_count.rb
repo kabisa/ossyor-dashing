595f85ed699ca7466042d2c2a445d9a3f9f7f36d
Matthijs Groen <matthijs.groen@gmail.com>
2013-11-28 11:00:04 +0100

    Update luminaire count widget for new XML filenames

--- jobs/luminaire_count.rb
+++ jobs/luminaire_count.rb
@@ -7,12 +7,27 @@ SCHEDULER.every '30m', :first_in => 0 do |job|
 
   namespaces = %w(HL1 HL2 HL3)
   luminaire_counts = namespaces.map do |namespace|
-    http = Net::HTTP.new('assets.pwl.philips.com.s3.amazonaws.com')
-    response = http.request(Net::HTTP::Get.new("/backend/ROOMS-#{namespace}-global-000900000000X.xml"))
-    luminaire_count = XmlSimple.xml_in(response.body, { 'ForceArray' => false })['catalog-item'].count
-  end.uniq.sort.reverse
+    begin
+      http = Net::HTTP.new('assets.pwl.philips.com.s3.amazonaws.com')
+      response = http.request(Net::HTTP::Get.new("/backend/ROOMS-#{namespace}-global-000900000000X.xml"))
+      XmlSimple.xml_in(response.body, { 'ForceArray' => false })['catalog-item'].count
+    rescue
+      nil
+    end
+  end
+  luminaire_counts += namespaces.map do |namespace|
+    begin
+      http = Net::HTTP.new('assets.pwl.philips.com.s3.amazonaws.com')
+      response = http.request(Net::HTTP::Get.new("/backend/ROOMS-#{namespace}-global-00090000.xml"))
+      XmlSimple.xml_in(response.body, { 'ForceArray' => false })['catalog-item'].count
+    rescue
+      nil
+    end
+  end
 
-  planned_count = ((((Date.today.year - 2013) * 12) + (Date.today.month - 1)) * 200) + 50
+  luminaire_counts = luminaire_counts.compact.uniq.sort.reverse
+
+  #planned_count = ((((Date.today.year - 2013) * 12) + (Date.today.month - 1)) * 200) + 50
 
   current_count = luminaire_counts[0]
   previous_count = luminaire_counts[1]
@@ -20,8 +35,8 @@ SCHEDULER.every '30m', :first_in => 0 do |job|
 
   send_event('luminaire_count', {
     current: current_count,
-    targetCount: planned_count,
-    difference:  current_count - planned_count,
+    #targetCount: planned_count,
+    #difference:  current_count - planned_count,
     lastReleased: last_released.abs,
     arrow: last_released > 0 ? 'icon-arrow-up' : 'icon-arrow-down'
   })
