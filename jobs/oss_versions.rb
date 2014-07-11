3ce7e8bb6f6bc82c4909ef0390d10a4706b8023b
wei.pien.lee <wei.pien.lee@philips.com>
2014-07-11 12:56:31 +0200

    Add exception handling

--- jobs/oss_versions.rb
+++ jobs/oss_versions.rb
@@ -9,20 +9,24 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
   versions = []
 
   environments.each do |environment|
-    http = Net::HTTP.new("www.#{environment}.philips.com")
-    response = http.request(Net::HTTP::Get.new('/'))
-    doc = Nokogiri::HTML(response.body)
-    doc.css('meta[name="PHILIPS.PWL.VERSION"]').each do |meta_tag|
-      version = meta_tag['content']
-      if version =~ /branch:/
-        version = version.match(/branch:\s+(?<version>.*)$/)[:version]
-      end
-      content = 'unknown'
-      doc.css('meta[name="PHILIPS.PWL.CONTENT-NAMESPACE"]').each do |namespace_meta_tag|
-        content = namespace_meta_tag['content']
-      end
+    begin
+      http = Net::HTTP.new("www.#{environment}.philips.com")
+      response = http.request(Net::HTTP::Get.new('/'))
+      doc = Nokogiri::HTML(response.body)
+      doc.css('meta[name="PHILIPS.PWL.VERSION"]').each do |meta_tag|
+        version = meta_tag['content']
+        if version =~ /branch:/
+          version = version.match(/branch:\s+(?<version>.*)$/)[:version]
+        end
+        content = 'unknown'
+        doc.css('meta[name="PHILIPS.PWL.CONTENT-NAMESPACE"]').each do |namespace_meta_tag|
+          content = namespace_meta_tag['content']
+        end
 
-      versions << { name: environment, version: version, content: content }
+        versions << { name: environment, version: version, content: content }
+      end
+    rescue StandardError => e
+      versions << { name: environment, version: e.message, content: 'ERROR' }
     end
   end
 
