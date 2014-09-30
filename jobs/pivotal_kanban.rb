a15e664f972f340376060b5b75ca3273816b522e
Matthijs Groen <matthijs.groen@gmail.com>
2014-09-30 07:23:59 +0200

    Add task progress

--- jobs/pivotal_kanban.rb
+++ jobs/pivotal_kanban.rb
@@ -5,11 +5,23 @@ ENVied.require
 client = TrackerApi::Client.new(token: ENVied.PIVOTAL_TRACKER_API_KEY)
 project  = client.project(ENVied.PIVOTAL_TRACKER_PROJECT)
 
-def to_story_json(story)
+def to_story_json(story, project)
+  #tasks = story.tasks
+  tasks = project.story(story.id).tasks
+  completed = tasks.select { |t| t.complete }
+  if tasks.length > 0
+    progress = ((completed.length.to_f / tasks.length) * 100).round
+  else
+    progress = '??'
+  end
+
   {
     title: story.name,
     kind: story.story_type,
-    owners: story.owners.map { |owner| owner.initials }
+    owners: story.owners.map { |owner| owner.initials },
+    #tasks: tasks.length,
+    #completed: completed.length,
+    progress: progress
   }
 end
 
@@ -27,13 +39,13 @@ SCHEDULER.every '3m', first_in: 0 do
   )
   upcoming_json = (rejected + upcoming).select do |story|
     story.labels.select { |label| label.name == 'impeded' }.empty?
-  end.map { |story| to_story_json story }[0...3]
+  end.map { |story| to_story_json story, project }[0...3]
 
   work_in_progress = project.stories(
     with_state: 'started',
     fields: story_fields
   )
-  work_in_progress_json = work_in_progress.map { |story| to_story_json story }
+  work_in_progress_json = work_in_progress.map { |story| to_story_json story, project }
 
   demo = project.stories(
     with_state: 'finished',
@@ -42,20 +54,20 @@ SCHEDULER.every '3m', first_in: 0 do
     with_state: 'delivered',
     fields: story_fields
   )
-  demo_json = demo.map { |story| to_story_json story }
+  demo_json = demo.map { |story| to_story_json story, project }
 
   done = project.stories(
     with_state: 'accepted',
     fields: story_fields,
     accepted_after: (DateTime.now - 7).iso8601
   ).reverse
-  done_json = done.map { |story| to_story_json story }
+  done_json = done.map { |story| to_story_json story, project }
 
   impeded = project.stories(
     with_label: 'impeded',
     fields: story_fields,
   )
-  impeded_json = impeded.map { |story| to_story_json story }
+  impeded_json = impeded.map { |story| to_story_json story, project }
 
   send_event('pivotal_upcoming', { stories: upcoming_json })
   send_event('pivotal_wip', { stories: work_in_progress_json })
